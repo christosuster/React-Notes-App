4d0398afb872c89e585b0f470bbefed361cbc936
Christos Uster <christos.uster@gmail.com>
2023-01-23 18:46:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setList] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  const changeText = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const addTask = () => {
+    const newTask = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskValue: inputText,
+    };
+
+    setList([...todoList, newTask]);
+  };
+
+  const deleteItem = (i) => {
+    const newList = todoList.filter((item) => {
+      return item.id !== i.id;
+    });
+
+    setList(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="myApp">
+        <header>MY NOTES APP</header>
+        <div className="inputField">
+          <div className="addItem">
+            <input type="text" onChange={changeText} id="textField" />
+            <button onClick={addTask}> ADD TASK </button>
+          </div>
+          <div className="items">
+            {todoList.map((item) => {
+              return (
+                <div className="listItem">
+                  <h3>{item.taskValue} </h3>
+                  <button onClick={() => deleteItem(item)}>X</button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <footer>A React project by Christos</footer>
+      </div>
     </div>
   );
 }
